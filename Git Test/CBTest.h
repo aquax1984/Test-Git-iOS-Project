d933f902b25e5c93dde93c51928f6c8c846fcdbc
Chris Bouquet <Chris@Chris-Bouquets-MacBook-Pro.local>
2014-03-02 10:06:34 -0700

    added a test class

---
+++ Git Test/CBTest.h
@@ -0,0 +1,13 @@
+//
+//  CBTest.h
+//  Git Test
+//
+//  Created by Chris Bouquet on 3/2/14.
+//  Copyright (c) 2014 Chris Bouquet. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CBTest : NSObject
+
+@end
---
+++ Git Test/CBTest.m
@@ -0,0 +1,13 @@
+//
+//  CBTest.m
+//  Git Test
+//
+//  Created by Chris Bouquet on 3/2/14.
+//  Copyright (c) 2014 Chris Bouquet. All rights reserved.
+//
+
+#import "CBTest.h"
+
+@implementation CBTest
+
+@end
